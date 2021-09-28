4ceaab93fae24b059a78cdc82e5e0ef8e2697317
JSON <jeisondh55@gmail.com>
2021-09-28 15:18:53 -0500

    Json salvando patria

--- src/App.js
+++ src/App.js
@@ -10,27 +10,17 @@ function App() {
 
 
     <><div className="App">
-      <a-scene vr-mode-ui="enabled: false">
+     <div className="spacer" />
+ 
+ <div id="scenewrapper">
+   <a-scene embedded vr-mode-ui="enabled: false">
+     <a-box position="0 0.5 -3" rotation="`0 ${360 * progress} 0`" color="#4CC3D9"></a-box>
+     <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+     <a-sky color="#ECECEC"></a-sky>
+   </a-scene>
+ </div>
 
-        <a-assets>
-          <a-asset-item id="Logo" src="/Sources/Models/Logo.obj"></a-asset-item>
-        </a-assets>
-
-        {/*    <!-- Camera -->*/}
-        <a-entity cam id="camera" camera="active:true; near:5.0; far:25000;" position="142.117 82.888 -111.378" rotation="-30.075 120.103 0"></a-entity>
-
-        {/*   <!-- <a-entity cam id="camera" camera="active:true; near:5.0; far:25000;"  position="64.869 42.476 -62.850" rotation="-28.075 124.103 0">-->*/}
-        {/*    <!-- scene -->*/}
-        <a-entity class="Nviroment" gltf-model="/Sources/Models/Scene.gltf" position="0 0 0" rotation="0 0 0"></a-entity>
-        {/*    <!-- Logo -->*/}
-        <a-entity class="Logo" obj-model="obj: #Logo; " material="emissive:#666; color:#fff" position="26.226 11.288 -29.544" rotation="76.991 125.373 10.059" scale="20.000 20.000 20.000"></a-entity>
-        {/*    <!-- luces -->*/}
-        <a-entity light="type: point; color: #c969ec; intensity: 0.800; distance: 100.010" position="28.903 4.781 -56.672"></a-entity>
-        <a-entity light="type: point; color: #53c1bf; intensity: 0.800; distance: 100.010" position="6.126 34.278 -21.932"></a-entity>
-        <a-entity light="type: point; color: #c969ec; intensity: 0.800; distance: 100.010" position="39.949 23.700 -7.648"></a-entity>
-
-        <a-sky color="#000000"></a-sky>
-      </a-scene>
+ <div className="spacer" />
     </div>
     
 
--- src/MovementCamera.js
+++ src/MovementCamera.js
@@ -3,39 +3,36 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const AFRAME = window.AFRAME;
 
+gsap.registerPlugin(ScrollTrigger)
+let _this = this
+let progress = 0;
+let triggerConfig = {
+  trigger: '#scenewrapper',
+  start: 'top center',
+  end: 'bottom center',
+  pin: false,
+  markers: true,
+  onToggle: self => console.log("toggled, isActive:", self.isActive),
+  onUpdate: self => {
+    progress = self.progress
+    console.log(progress);
+    // console.log("progress:", _this.name, self.progress.toFixed(3), "direction:", self.direction, "velocity", self.getVelocity());
+  }
+}
+
+ScrollTrigger.create(triggerConfig);
+
 AFRAME.registerComponent('cam', {
 
-    init: function () {
+  init: function () {
+    console.log("regstri");
+    //posicion de camera
+    var cameraPos = document.querySelector('#camera');
 
-
-        //posicion de camera
-        var cameraPos = document.querySelector('#camera');
-
-        //creacion de timeline
-        var tl = gsap.timeline({repeat:0});
-        //Movimiento de camara
-       tl.to(cameraPos.object3D.position,{x:44.009 , y:27.733, z:-55.819,duration:3, ease: "power2.inOut"},0)
-
-     //gsap 
-gsap.registerPlugin(ScrollTrigger);
-//set camera position
-
-
-const timeline = gsap.timeline(cameraPos.object3D.position,{ 
-  scrollTrigger: {
-    trigger: "#App",
-
-   start: 'top top',
-    end: '+=500%',
-    pin: true,
-    scrub: true 
-  }
-})
-
-     }
-    });
-
-
-
-
-
+    //creacion de timeline
+    var tl = gsap.timeline({ repeat: 0 });
+    //Movimiento de camara
+    tl.to(cameraPos.object3D.position, { x: 44.009, y: 27.733, z: -55.819, duration: 3, ease: "power2.inOut" }, 0);
+  },
+  //gsap
+});
